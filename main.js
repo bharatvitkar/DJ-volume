44477b49b63bfaf4c38a55963ec4663829ec5608
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-10-15 09:36:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
 function preload()
 {
 song=loadSound("music.mp3");
@@ -10,6 +14,28 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+  
+} 
+
+function gotPoses(results)
+{
+  if(results.length>0)
+  {
+    console.log(results);
+    leftwristx=results[0].pose.leftWrist.x;
+    leftwristy=results[0].pose.leftWrist.y;
+    console.log("leftwristx = "+leftwristx+"leftwristy = "+leftwristy);
+   rightwristx=results[0].pose.rightWrist.x;
+    rightwristy=results[0].pose.rightWrist.y;
+    console.log("rightwristx = "+rightwristx+"rightwristy = "+rightwristy);
+  }
+}
+
+function modelLoaded()
+{
+  console.log('poseNet is initialized');
 }
 
 function draw()
@@ -20,5 +46,8 @@ function draw()
 function play()
 {
   song.play();
+ song.setVolume(1);
+ song.rate(1);
 }
+
 
